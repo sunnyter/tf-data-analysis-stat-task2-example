6fb4489a12c85b92cb0680e36feefacf694dc435
sunnyter <129841599+sunnyter@users.noreply.github.com>
2023-04-06 13:35:20 +0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,5 +11,5 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     scale = np.sum(np.square(x))
-    return scale / (13*chi2.ppf(1 - alpha / 2, df=len(x))), \
-           scale / (13*chi2.ppf(alpha / 2, df=len(x)))
+    return np.sqrt(scale / (13*chi2.ppf(1 - alpha / 2, df=len(x)))), \
+           np.sqrt(scale / (13*chi2.ppf(alpha / 2, df=len(x))))
